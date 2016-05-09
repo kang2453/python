262362d472403c1c1224322abf0356a52d17921f
kang2453 <kang2453@cdlp.somansa.com>
2016-05-09 11:08:15 +0900

    server, client 첫번째 커밋

---
+++ Server.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# python3
+
+
+from threading import Thread
+import time
+import sys
+import asyncio
+import asyncio.streams
+
+def RecvThread():
+	pass
+
+
+
+
+def  SendThread():
+	pass
+
+
+def MsgHandle():
+	pass
+
+
+
+
+
+
+if __name__ == '__main__':
+	print("hello, world")
+	pass
---
+++ client.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from threading import Thread
+import time
